c0ce374c600594c9c4ede7ecd3880ee93a572b0e
Samuel Goodwin <samuel@roundwallsoftware.com>
2019-05-28 12:47:21 +0200

    Initial commit.

---
+++ SharingTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SharingTest
+//
+//  Created by Samuel Goodwin on 5/28/19.
+//  Copyright © 2019 Samuel Goodwin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SharingTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SharingTest
+//
+//  Created by Samuel Goodwin on 5/28/19.
+//  Copyright © 2019 Samuel Goodwin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
